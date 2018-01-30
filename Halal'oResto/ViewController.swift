680ab18f9419d9e518c4202dda391a15e3da81d1
sadokiut <benfreds@iut1i-605-a10.local>
2018-01-30 09:01:18 +0100

    core

---
+++ Halal'oResto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Halal'oResto
+//
+//  Created by Sadok Ben Fredj on 30/01/2018.
+//  Copyright © 2018 Sadok Ben Fredj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
