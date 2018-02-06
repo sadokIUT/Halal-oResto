b28398779f14861a23e7af0d996559c1ad157fe1
kevinGRASSET <grassetk@iut1i-605-a16.local>
2018-02-06 12:13:28 +0100

    nouvelle view

--- Halal'oResto/Controller/ViewController.swift
+++ Halal'oResto/Controller/ViewController.swift
@@ -12,41 +12,31 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var nomPlat1: UILabel!
+    @IBOutlet weak var nomPlat1: UILabel?
     @IBOutlet weak var Resto1: UIView!
     let r = Restaurant(horraireDebut:8, horraireFin:15,telephone:"0645789652",adresse:"14 rue brocherie",logo2: #imageLiteral(resourceName: "logoRest"))
     let p = Plat(nom2: "assiete tikka massala", menu2: false, prix2: 8.0, image2: #imageLiteral(resourceName: "image12"))
-
-   
     
    
-    
-    @IBAction func tapChickeAlpes() {
-        //loadResto(resto:r)
-         r.ajouterPlat(plat: p)
+    @IBAction func Click(_ sender: Any)
+    {
         
-        do {
-           let np = try self.nomPlat1.text = "Text"
-        } catch SandwichError.NotMe {
-            print("Not me error")
-        } catch SandwichError.DoItYourself {
-            print("do it error")
-        } catch let error {
-            print(error.localizedDescription)
-        }
-       
         
+   
+        let test = SecondViewController()
         
     }
+    
+   
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        func loadResto(resto:Restaurant){
-            self.nomPlat1.text = resto.plats[0].nom
-        }
+       
         
     }
 
@@ -54,6 +44,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    
+    
+    
+
+
 }
 
 
---
+++ Halal'oResto/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Halal'oResto
+//
+//  Created by Kevin Grasset on 06/02/2018.
+//  Copyright © 2018 Sadok Ben Fredj. All rights reserved.
+//
+
+import Foundation
+
+
+let    r = Restaurant(horraireDebut:8, horraireFin:15,telephone:"0645789652",adresse:"14 rue brocherie",logo2: #imageLiteral(resourceName: "logoRest"))
+let p = Plat(nom2: "assiete tikka massala", menu2: false, prix2: 8.0, image2: #imageLiteral(resourceName: "image12"))
+
